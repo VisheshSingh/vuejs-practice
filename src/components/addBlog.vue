4a141e118c3bd3b5767510cb1774bdf9d474964f
Vishesh <visheshsingh16@gmail.com>
2018-09-09 14:33:32 -0500

    Select boxes

--- src/components/addBlog.vue
+++ src/components/addBlog.vue
@@ -17,6 +17,10 @@
             <label>Yoda</label>
             <input type="checkbox" value="yoda" v-model="blog.categories" />
         </div>
+        <label>Author:</label>
+        <select v-model="blog.author">
+            <option v-for="author in authors">{{author}}</option>
+        </select>
         <div id="preview">
             <h3>Preview blog</h3>
             <p>Blog title: {{ blog.title }}</p>
@@ -26,6 +30,7 @@
             <ul>
                 <li v-for="category in blog.categories">{{category}}</li>
             </ul>
+            <p>Blog Author: {{blog.author}}</p>
         </div>
     </div>
 </template>
@@ -38,8 +43,10 @@ export default {
       blog: {
         title: "",
         content: "",
-        categories: []
-      }
+        categories: [],
+        author: ""
+      },
+      authors: ["The Net Ninja", "The Angular Avenger", "The Vue Vindicator"]
     };
   },
   methods: {}
