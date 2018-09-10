59125a6c7541b02787db3733dca722cb20d17c19
Vishesh <visheshsingh16@gmail.com>
2018-09-09 20:38:00 -0500

    Posting to Firebase

--- src/components/addBlog.vue
+++ src/components/addBlog.vue
@@ -55,11 +55,7 @@ export default {
   methods: {
     post: function() {
       this.$http
-        .post("https://jsonplaceholder.typicode.com/posts", {
-          title: this.blog.title,
-          body: this.blog.content,
-          userId: 1
-        })
+        .post("https://vue-playlist-25566.firebaseio.com/posts.json", this.blog)
         .then(data => {
           console.log(data);
           this.submitted = true;
